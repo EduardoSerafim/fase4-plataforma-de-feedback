ea6ec15cf7ff91855a33780b4df5bbd843197157
ASchneider12 <schn.alessandro@gmail.com>
2025-12-10 21:51:22 -0400

    postman

--- src/main/java/br/com/fiap/feedback/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/feedback/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.POST, "/avaliacao").permitAll()
+                .requestMatchers(HttpMethod.GET, "/avaliacao").permitAll()
                 .requestMatchers("/avaliacao/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
         );
